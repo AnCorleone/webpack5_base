7f174afac9c8221414e4de8c667487888f805da5
张安然 <anakin@newsupplytech.com>
2022-09-24 19:18:18 +0800

    开发环境增加css压缩插件

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -2,6 +2,7 @@ const { resolve } = require('path')
 const ESLintPlugin = require('eslint-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 function getStyleLoader(pre) {
   return [
     // 不同于dev,prod使用 ‘MiniCssExtractPlugin.loader’,会将css提取为单独的文件,并将多个css文件单独提取为一个文件
@@ -70,6 +71,13 @@ module.exports = {
       }
     ]
   },
+  optimization: {
+    minimizer: [
+      // 在 webpack@5 中，你可以使用 `...` 语法来扩展现有的 minimizer（即 `terser-webpack-plugin`），将下一行取消注释
+      // `...`,
+      new CssMinimizerPlugin()
+    ]
+  },
   plugins: [
     // eslint 插件,需要配合.eslintrc.js 文件,.eslintignore 忽略文件,vscode eslint插件
     new ESLintPlugin({ context: resolve(__dirname, '../src') }),
@@ -77,6 +85,8 @@ module.exports = {
       // 配置要打包的html 文件的位置 ,会自动引入打包需要的资源
       template: resolve(__dirname, '../public/index.html')
     }),
+    // 这个插件使用 cssnano 优化和压缩 CSS。
+    new MiniCssExtractPlugin(),
     // 本插件会将 CSS 提取到单独的文件中，为每个包含 CSS 的 JS 文件创建一个 CSS 文件
     new MiniCssExtractPlugin({
       filename: 'static/css/main.css'
